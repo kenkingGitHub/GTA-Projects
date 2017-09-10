1f2003e54a42d006af0aab8a39a1c43fbf8dc033
kenkingGitHub <kenking@rambler.ru>
2017-09-10 14:35:07 +0600

    update 10.09.2017

--- III_AdditionalComponents/III_AdditionalComponents.cpp
+++ III_AdditionalComponents/III_AdditionalComponents.cpp
@@ -17,9 +17,10 @@ public:
     class VehicleComponents { // Класс, который представляет наши данные (можно сказать, что эти данные "прикрепляются" к структуре транспорта)
     public:
         RwFrame *m_pBootSliding;
-        
+        RwFrame *m_pRealRul;
+
         VehicleComponents(CVehicle *) { // Конструктор этого класса будет вызван при вызове конструктора транспорта (CVehicle::CVehicle)
-            m_pBootSliding = nullptr;  // устанавливаем все указатели в 0
+            m_pBootSliding = m_pRealRul = nullptr;  // устанавливаем все указатели в 0
         }
     };
 
@@ -29,12 +30,13 @@ public:
 
         Events::vehicleSetModelEvent += [](CVehicle *vehicle, int modelIndex) { // Выполняем нашу функцию, когда игра устанавливает модель транспорту
             if (vehicle->m_pRwClump) { // Если создан графический обьект модели (RpClump)
-                vehComps.Get(vehicle).m_pBootSliding = CClumpModelInfo::GetFrameFromName(vehicle->m_pRwClump, "boot_sliding"); // Находим компоненты в иерархии
-                                                                                                                                // и записываем их в наш класс
+                vehComps.Get(vehicle).m_pBootSliding = CClumpModelInfo::GetFrameFromName(vehicle->m_pRwClump, "boot_sliding"); // Находим компоненты в иерархии и записываем их в наш класс
+                vehComps.Get(vehicle).m_pRealRul = CClumpModelInfo::GetFrameFromName(vehicle->m_pRwClump, "realrul"); 
+                                                                                                                                
                 
             }
-            else // иначе устанавливаём всё в 0
-                vehComps.Get(vehicle).m_pBootSliding = nullptr;
+            else  // иначе устанавливаём всё в 0
+                vehComps.Get(vehicle).m_pBootSliding = vehComps.Get(vehicle).m_pRealRul = nullptr;
         };
 
         Events::drawingEvent += [] {
@@ -54,32 +56,42 @@ public:
                                 automobile->m_carDamage.SetDoorStatus(BOOT, 3);
                         }
                     }
-                    
+                    // руль
+                    if (vehComps.Get(vehicle).m_pRealRul) {
+                        CMatrix m(&vehComps.Get(vehicle).m_pRealRul->modelling, false);
+                        m.SetRotateYOnly(vehicle->m_fSteerAngle * (-7.0f));
+                        m.UpdateRW();
+                    }
+                    // 
+
                 }
             }
 
 
-            //CVehicle *vehicle = FindPlayerVehicle();
-            //if (vehicle && vehicle->m_nVehicleClass == VEHICLE_AUTOMOBILE) {
-            //    CAutomobile *automobile = reinterpret_cast<CAutomobile *>(vehicle);
-            //    if (automobile->m_aCarNodes[CAR_BOOT]) {
-            //        // вывод текста
-            //        CFont::SetScale(0.5f, 1.0f);
-            //        CFont::SetColor(CRGBA(255, 255, 255, 255));
-            //        CFont::SetJustifyOn();
-            //        CFont::SetFontStyle(0);
-            //        CFont::SetPropOn();
-            //        CFont::SetWrapx(600.0f);
-            //        wchar_t text[32];
-            //        swprintf(text, L"boot %d", automobile->m_carDamage.GetDoorStatus(BOOT));
-            //        CFont::PrintString(10.0f, 10.0f, text);
-            //        swprintf(text, L"wing %d", automobile->m_carDamage.GetPanelStatus(WING_REAR_LEFT));
-            //        CFont::PrintString(10.0f, 30.0f, text);
-            //        swprintf(text, L"angle %.2f", automobile->m_aDoors[1].m_fAngle);
-            //        CFont::PrintString(10.0f, 50.0f, text);
-            //    }
+            CVehicle *vehicle = FindPlayerVehicle();
+            if (vehicle && vehicle->m_nVehicleClass == VEHICLE_AUTOMOBILE) {
+                CAutomobile *automobile = reinterpret_cast<CAutomobile *>(vehicle);
+                //if (automobile->m_aCarNodes[CAR_BOOT]) {
+                    // вывод текста
+                    CFont::SetScale(0.5f, 1.0f);
+                    CFont::SetColor(CRGBA(255, 255, 255, 255));
+                    CFont::SetJustifyOn();
+                    CFont::SetFontStyle(0);
+                    CFont::SetPropOn();
+                    CFont::SetWrapx(600.0f);
+                    wchar_t text[32];
+                    swprintf(text, L"steer angle %.2f", vehicle->m_fSteerAngle);
+                    CFont::PrintString(10.0f, 10.0f, text);
+                    
+                    /*swprintf(text, L"boot %d", automobile->m_carDamage.GetDoorStatus(BOOT));
+                    CFont::PrintString(10.0f, 10.0f, text);
+                    swprintf(text, L"wing %d", automobile->m_carDamage.GetPanelStatus(WING_REAR_LEFT));
+                    CFont::PrintString(10.0f, 30.0f, text);
+                    swprintf(text, L"angle %.2f", automobile->m_aDoors[1].m_fAngle);
+                    CFont::PrintString(10.0f, 50.0f, text);*/
+                //}
 
-            //}
+            }
 
         };
 
